91feb64cc78a000cad6bf25eec70ae2b66bc1e10
hieunm2 <harrynguyen610@gmail.com>
2025-03-03 19:32:16 +0700

    init commit

---
+++ src/main/java/com/mods/devteriaspring/DevteriaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mods.devteriaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevteriaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevteriaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mods/devteriaspring/DevteriaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mods.devteriaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevteriaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
